4f1654e91e61862a4fd0e995942fda5292ce5b63
yeddu.prudvi <yeddu.prudvi@PUN-LAP-YEDDU>
2023-01-19 13:40:36 +0530

    Add project files.

---
+++ EmployeeDetails/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeDetails.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDetails.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        }  
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeDetails/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeDetails.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+    }
+}
